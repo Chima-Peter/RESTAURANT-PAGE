5239a844fac8560643c80561537b3665db10bc71
OJIMMA CHIMA PETER <chimaojimma@gmail.com>
2024-02-05 23:28:15 +0100

    code works

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,7 @@ const { merge } = require('webpack-merge');
 module.exports = merge(common, {
     mode: 'production',
     output: {
-        filename: 'main.[contenthash].js',
+        filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
